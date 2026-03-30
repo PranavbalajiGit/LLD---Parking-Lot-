d83d5ad84344be126b0cf4a7f904d812897b81ee
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-03-30 12:27:20 +0530

    Parking fare Implemented Using Strategy Design Pattern

---
+++ src/ParkingFare/ConcreteFare/BasicHourlyFare.java
@@ -0,0 +1,32 @@
+package ParkingFare.ConcreteFare;
+
+import CommonEnum.DurationType;
+import ParkingFare.ParkingFeeStartegy;
+
+public class BasicHourlyFare implements ParkingFeeStartegy {
+
+    @Override
+    public double calculateFee(String vehicleType, int duration, DurationType durationType) {
+        switch (vehicleType.toLowerCase()) {
+            case "car":
+                return durationType == DurationType.HOURS
+                        ? duration * 10.0
+                        : duration * 10.0 * 24;
+
+            case "bike":
+                return durationType == DurationType.HOURS
+                        ? duration * 5.0
+                        : duration * 5.0 * 24;
+
+            case "auto":
+                return durationType == DurationType.HOURS
+                        ? duration * 8.0
+                        : duration * 8.0 * 24;
+
+            default:
+                return durationType == DurationType.HOURS
+                        ? duration * 15.0
+                        : duration * 15.0 * 24;
+        }
+    }
+}
--- src/ParkingFare/ParkingFeeStartegy.java
+++ src/ParkingFare/ParkingFeeStartegy.java
@@ -1,4 +1,8 @@
 package ParkingFare;
 
-public class ParkingFeeStartegy {
+
+import CommonEnum.DurationType;
+
+public interface ParkingFeeStartegy {
+    double calculateFee(String vehicleType , int duration , DurationType durationType);
 }
